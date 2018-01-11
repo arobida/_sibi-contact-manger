84d039f36ee2fb789e13475a59e40bcc87dfdd63
Andrew Robida <afrobida@gmail.com>
2018-01-11 22:49:30 +0000

    mirror sibi-adder-app styling

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
 import {Button, Icon} from 'semantic-ui-react';
 import './App.css';
+import sibi from './sibi.png';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+        <img  src={sibi} style={{width:'30%',margin:'8px'}} />
           <h1 className="App-title">Sibi Contacts Manager</h1>
         </header>
-        <Button secondary>
-        It works
-        </Button>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
