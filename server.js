21bd1808c66b2b19b15358ced9cd18879726cad6
Andrew Robida <afrobida@gmail.com>
2018-01-12 03:43:13 +0000

    added db connection

--- server.js
+++ server.js
@@ -10,6 +10,23 @@ const PORT = normalizePort(process.env.PORT || 5000);
 const app = express();
 const dev = app.get('env') !== 'production';
 
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
 if(!dev) {
     app.disable('x-powered-by');
     app.use(compression());
