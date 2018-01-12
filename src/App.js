c384dcfe5676a48f4574ea5744f9598f32a30278
Andrew Robida <afrobida@gmail.com>
2018-01-12 05:03:55 +0000

    search mock up

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {Button, Icon} from 'semantic-ui-react';
 import './App.css';
 import sibi from './sibi.png';
 
+import SearchContacts from './components/SearchContacts';
+
 class App extends Component {
   render() {
     return (
@@ -11,6 +13,7 @@ class App extends Component {
         <img  src={sibi} style={{width:'30%',margin:'8px'}} />
           <h1 className="App-title">Sibi Contacts Manager</h1>
         </header>
+        <SearchContacts/>
       </div>
     );
   }
